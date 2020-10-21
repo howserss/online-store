dfcfd8936be17ad6f4e7e95c72c1be2e11fc6818
howserss <howserss@gmail.com>
2020-10-21 12:30:09 -0400

    new branch online-store.

---
+++ src/app/product/product.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { PRODUCTS } from "./../store/market";
+import { Product } from "./../store/products.model"
+import { ActivatedRoute } from "@angular/router";
+import { Store } from "@ngrx/store";
+import * as Cart from "./../store/actions";
+
+@Component({
+  selector: 'app-product',
+  templateUrl:'./product.component.html',
+  styles: [ ]
+})
+
+export class ProductComponent implements OnInit {
+
+  product:Product
+
+  constructor(private route: ActivatedRoute, private store: Store<any>) { }
+
+  ngOnInit() {
+    this.route.params.subscribe((p)=>{
+        let id = p['id']
+        let result = Array.prototype.filter.call(PRODUCTS,(v)=>v.id == id)
+        if (result.length > 0) {
+          this.product = result[0]
+        }
+    })
+  }
+
+  addToCart(product) {
+        this.store.dispatch(new Cart.AddProduct(product))
+  }
+}
